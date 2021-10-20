e9d46cc19ce07b5a25289208c3da32d1f76648cb
zkirsner <zkirsner@umich.edu>
2021-10-19 20:15:29 -0400

    finished product

--- regex.py
+++ regex.py
@@ -23,44 +23,55 @@ def find_word(string_list):
     """ Return a list of words that contain three digit numbers in the middle. """
 
     # initialize an empty list
-
+    word_list = []
     # define the regular expression
-
+    reg_ex = r'\b([A-Za-z]+)\d{3}([A-Za-z]+)'
     # loop through each line of the string list 
-
+    for line in string_list:
+        a = re.findall(reg_ex, line)
+        for i in a:
+            word_list.append(i)
+    return word_list
     # find all the words that match the regular expression in each line
-    
+
     # loop through the found words and add the words to your empty list 
 
     #return the list of all words that start with the letter B, E, or T
-    pass
 
 
 def find_days(string_list):
     """ Return a list of days from the list of strings the dates format in the text are MM/DD/YYYY. """  
 
     # initialize an empty list
-
+    days_list = []
     # define the regular expression
-
+    reg_ex = r'(\b\d{1,2}[\/](\d{1,2})[\/](\d{4})\b)'
     # loop through each line of the string list
-    
+    for line in string_list:
+        b = re.findall(reg_ex, line)
+        for i in b:
+            days_list.append(i[1])
+    return days_list
     # find all the dates that match the regular expression in each line
     
     # loop through the found dates and only add the days to your empty list 
     
     #return the list of days
-    pass
 
 def find_domains(string_list):
     """ Return a list of web address domains from the list of strings the domains of a wbsite are after www. """
 
     # initialize an empty list
-
+    domain_list = []
     # define the regular expression
-
+    reg_ex = r'https?://[\w.]+'
     # loop through each line of the string list
-
+    for line in string_list:
+        c = re.findall(reg_ex, line)
+        for i in c:
+            domain_name = i.split('//')[1].strip('www.')
+            domain_list.append(domain_name)
+    return domain_list
     # find all the domains that match the regular expression in each line
 
     # loop through the found domains
@@ -71,8 +82,6 @@ def find_domains(string_list):
     # add the domains to your empty list
     
     #return the list of domains
-    pass
-
 class TestAllMethods(unittest.TestCase):
 
 
